6063bcbda1232946f60492181f9bcf2cc5fae09b
Cody Shimizu <chompybird75@gmail.com>
2025-12-29 23:09:24 -0800

    Remove log function from log.lua

--- src/log.lua
+++ src/log.lua
@@ -1,13 +1,1 @@
--- simple lua chat logger
 
-function log(source, content)
-    local file = io.open("logs.txt", "w")
-    if file then
-        local timestamp = os.date("%Y-%m-%d %H:%M:%S")
-        local clean = content:gsub("/n", " ")
-        file:write(string.format("[%s] %s: %s\n", timestamp, source, clean))
-        file:close()
-        return true
-    end
-    return false
-end
